2039b1ddad6fa006ed63f65c80b0a4a05a708bb8
Sebastien Baudray <baudray.sebastien@gmail.com>
2022-01-02 19:45:27 +0100

    Task 1: Counter

---
+++ src/routes/counter.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Counter</title>
+</svelte:head>
+
+<Counter />
